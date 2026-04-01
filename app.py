f8bbcc1d26d12e92f79bdf22d3f66a9513c2642d
Tejaswini691616 <tejaswinip1123@gmail.com>
2026-04-01 11:22:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_comments():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "success message",
+        "status": "singing songs",
         "status_code": 200
     })
 
